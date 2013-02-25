cb69264e254d7bfa36021617258ae31ece205d8e
Yi Lin <yi.lin@anu.edu.au>
2013-02-25 12:36:48 +1100

    a more complicated address test

--- test/org/rjava/test/magic/AddressTest.java
+++ test/org/rjava/test/magic/AddressTest.java
@@ -3,10 +3,12 @@ package org.rjava.test.magic;
 import org.rjava.restriction.rulesets.RJavaCore;
 import org.vmmagic.unboxed.Address;
 import org.vmmagic.unboxed.ObjectReference;
+import org.vmmagic.unboxed.Offset;
 
 @RJavaCore
 public class AddressTest {
-
+    public static final Offset RJAVA_ARRAY_C_HEADER = Offset.fromIntSignExtend(12);
+    public static final Offset INT_WIDTH = Offset.fromIntSignExtend(4);
     /**
      * @param args
      */
@@ -16,6 +18,21 @@ public class AddressTest {
         i_address.store(2);
         int after = i_address.loadInt();
         System.out.println(after);
+        
+        final Offset RJAVA_ARRAY_C_HEADER = Offset.fromIntSignExtend(12);
+        final Offset INT_WIDTH = Offset.fromIntSignExtend(4);
+        
+        int[] array = new int[5];
+        Address array_addr = ObjectReference.fromObject(array).toAddress();
+        array_addr = array_addr.plus(RJAVA_ARRAY_C_HEADER);
+        array_addr.store(0);
+        for (int j = 1; j < 5; j++) {
+            array_addr = array_addr.plus(INT_WIDTH);
+            array_addr.store(j);
+        }
+        for (int k = 0; k < 5; k++) {
+            System.out.println(array[k]);
+        }
     }
 
 }
