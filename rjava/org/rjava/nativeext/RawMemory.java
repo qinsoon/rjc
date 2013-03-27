56cb82136466b89d6afd0988c8afed3ed9700746
Yi Lin <yi.lin@anu.edu.au>
2013-03-27 18:51:59 +1100

    org.rjava.ext.RawMemory now has malloc/mmap/mprotect. Unit test passed

--- rjava/org/rjava/nativeext/RawMemory.java
+++ rjava/org/rjava/nativeext/RawMemory.java
@@ -1,7 +1,36 @@
 package org.rjava.nativeext;
 
 import org.vmmagic.unboxed.Address;
+import org.vmmagic.unboxed.Extent;
+import org.vmmagic.unboxed.Offset;
 
 public abstract class RawMemory {
     public static native Address malloc(int size);
+    
+    // constants for protection and mapping calls
+    public static final int PROT_NONE = 0;
+    public static final int PROT_READ = 1;
+    public static final int PROT_WRITE = 2;
+    public static final int PROT_EXEC = 4;
+
+    public static final int MAP_PRIVATE = 2;
+    public static final int MAP_FIXED     = 16; 
+            //(VM.BuildForLinux) ? 16 : (VM.BuildForOsx) ?     16 : (VM.BuildForSolaris) ? 0x10 :256;
+    public static final int MAP_ANONYMOUS = 0x1000; 
+            //(VM.BuildForLinux) ? 32 : (VM.BuildForOsx) ? 0x1000 : (VM.BuildForSolaris) ? 0x100 : 16;
+    
+    public static final int MAP_FAILED = -1;
+    
+    public static native Address mmap(
+            Address start, 
+            Extent length, 
+            int protection, 
+            int flags, 
+            int fd, 
+            Offset offset);
+    
+    public static native int mprotect(
+            Address start, 
+            Extent length, 
+            int prot);
 }
--- unittest/src/org/rjava/unittest/ext/TestRawMemory.java
+++ unittest/src/org/rjava/unittest/ext/TestRawMemory.java
@@ -3,16 +3,45 @@ package org.rjava.unittest.ext;
 import org.rjava.nativeext.RawMemory;
 import org.rjava.unittest.UnitTest;
 import org.vmmagic.unboxed.Address;
+import org.vmmagic.unboxed.Extent;
+import org.vmmagic.unboxed.Offset;
 
 public class TestRawMemory extends UnitTest {
 
     public static void main(String[] args) {
         start("Test malloc");
         check(testMalloc());
+        
+        start("Test mmap");
+        check(testMMap());
+        
+        start("Test mprotect");
+        check(testMProtect());
     }
     
     public static boolean testMalloc() {
         Address addr = RawMemory.malloc(500);
         return !addr.isZero();
+    }
+    
+    public static boolean testMMap() {
+        Address ret = RawMemory.mmap(Address.zero(), 
+                Extent.fromIntZeroExtend(4096), 
+                RawMemory.PROT_READ | RawMemory.PROT_WRITE, 
+                RawMemory.MAP_ANONYMOUS | RawMemory.MAP_PRIVATE, -1, Offset.zero());
+        return ret.toInt() != RawMemory.MAP_FAILED;
+    }
+    
+    public static boolean testMProtect() {
+        Extent length = Extent.fromIntZeroExtend(4096);
+        Address ret = RawMemory.mmap(Address.zero(), 
+                length, 
+                RawMemory.PROT_READ, 
+                RawMemory.MAP_ANONYMOUS | RawMemory.MAP_PRIVATE, -1, Offset.zero());
+        
+        if (ret.toInt() == RawMemory.MAP_FAILED)
+            return false;
+        
+        return RawMemory.mprotect(ret, length, RawMemory.PROT_READ | RawMemory.PROT_WRITE) == 0;
     }
 }
